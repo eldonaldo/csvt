f28de4e76cb26787e0edb6dd2863183c9cbf9c27
Nico Previtali <nico.previtali@gmail.com>
2021-12-29 19:01:31 +0100

    root command removed

--- cmd/root.go
+++ cmd/root.go
@@ -22,8 +22,6 @@ THE SOFTWARE.
 package cmd
 
 import (
-	"fmt"
-	"github.com/eldonaldo/csvt/internal/core"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -41,10 +39,7 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	Run: func(cmd *cobra.Command, args []string) {
-		d := core.ReadFromStdIn()
-		fmt.Println(d)
-	},
+	// Run: func(cmd *cobra.Command, args []string) {},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
